b01205ae791ee202bae2fbb63872b6ab870fb58d
gigiopontoglio <giovannippontoglio@gmail.com>
2024-11-11 11:18:06 -0300

    Add files via upload

---
+++ MauiAppHotel-master/MauiAppHotel-master/MauiAppHotel/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MauiAppHotel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // Definindo rotas para as páginas adicionais
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+            Routing.RegisterRoute(nameof(ReservaPage), typeof(ReservaPage));
+            Routing.RegisterRoute(nameof(PerfilPage), typeof(PerfilPage));
+        }
+    }
+}
---
+++ MauiAppHotel-master/MauiAppHotel-master/MauiAppHotel/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MauiAppHotel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            // Validação simples para não permitir datas passadas
+            if (e.NewDate < DateTime.Now.Date)
+            {
+                DisplayAlert("Data inválida", "Selecione uma data futura.", "OK");
+                ((DatePicker)sender).Date = DateTime.Now.Date; // Reseta para a data atual
+            }
+            else
+            {
+                // Lógica adicional para datas válidas, se necessário
+            }
+        }
+    }
+}
